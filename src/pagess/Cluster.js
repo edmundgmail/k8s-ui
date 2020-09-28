76caca0f1380c8186d00b169c5bc3e3f4ad812cf
edmund guo <edmund guo>
2020-09-28 11:14:24 -0400

    Cluster management

---
+++ src/pagess/Cluster.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Cluster = (cluster) => (
+    <div></div>
+)
--- src/pagess/Setting.js
+++ src/pagess/Setting.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import {ListGroup} from 'react-bootstrap';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Table from 'react-bootstrap/Table'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import { Pencil, Trash, Plus } from 'react-bootstrap-icons';
+import {Cluster} from './Cluster';
+import styled from 'styled-components';
+const Styles = styled.table`
+.center {
+    text-align: center;
+}
+`
+export const  Setting = () => {
+    const [clusters, setClusters] = useState([])
 
-export const  Setting = () => (
-<ListGroup>
-  <ListGroup.Item>Cras justo odio</ListGroup.Item>
-  <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-  <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-  <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-</ListGroup>
-)
+    useEffect(()=>{
+        axios.get(`/clusters`)
+        .then(results => setClusters(results.data))
+        .catch(error=>console.log(error))
+     })
+
+     const deleteCluster = (cluster) => {
+         setClusters(clusters.filter(item=>item.name !== cluster.name))   
+     }
+
+     const editCluster = (cluster) => Cluster(cluster)
+
+     const populateRow = (cluster) => (
+        <tr>
+        <td>{cluster.name}</td>
+        <td>{cluster.url}</td>
+        <td>        
+                <ButtonGroup class="sm">
+                <Button onClick={editCluster(cluster)}><Pencil/></Button>
+                <Button onClick={e=>deleteCluster(cluster)}><Trash/></Button>
+                </ButtonGroup>
+            </td>
+        </tr>
+
+     )
+
+    return (
+    <Styles>    
+     <Card>
+    <Button><Plus/>Add New Cluster</Button>
+    <Table center striped bordered hover size="sm">
+    <thead>
+        <tr>
+        <th>Name</th>
+        <th>Url</th>
+        <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        {clusters.map(cluster=>populateRow(cluster))}        
+    </tbody>
+    </Table>
+    </Card>   
+    </Styles>)
+
+}
